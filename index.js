7bf2acc88849fd3529ae1a9b9489e35849893e46
proohit <direnc99@gmail.com>
2021-07-01 23:23:21 +0200

    rework

--- index.js
+++ index.js
@@ -1,88 +1,141 @@
 #!/usr/bin/env node
 
-/**
- * can have 3 arguments
- * 1. argument is the directory path
- * 2. argument is the name of each file
- * 3. season number
- */
 import fs from 'fs';
-import { exit } from 'process';
-import promptSync from 'prompt-sync';
+import prompts from 'prompts';
+import { program } from 'commander';
 
-const prompt = promptSync({ sigint: true });
+const FILE_EXTENSIONS = ['.mkv', '.avi', '.mp4'];
+const FILE_EXTENSIONS_REG = new RegExp(FILE_EXTENSIONS.join('|'));
+const EP_REG_FULL = RegExp(/(e|E)(?<episode>\d{1,})/);
+const EP_REG_IMPLICIT = /(\_|\.)(?<episode>\d{1,})(\_|\.)/;
+const EP_REGS = [EP_REG_FULL, EP_REG_IMPLICIT];
 
-const fileEndings = ['avi', 'mkv', 'mp4'];
-const args = process.argv.slice(2, process.argv.length);
+program
+  .option('-i, --interactive')
+  .option('-f, --folder <folder>')
+  .option('-n, --name <name>')
+  .option('-s, --season <season>')
+  .option('-p, --preview')
+  .parse();
 
-const isInteractive = !!args.find(
-  (arg) => arg === '--interactive' || arg === '-i'
-);
+let opts = program.opts();
 
-let dir, name, season, epOffset;
-
-if (isInteractive) {
-  dir = prompt('Video folder path:');
-  name = prompt('Series name:');
-  season = pad(prompt('Season:'), 2);
-  epOffset = Number(prompt('offset:'));
+if (opts.interactive) {
+  runInteractively();
 } else {
-  const dirArg = args.findIndex((arg) => arg === '--folder' || arg === '-f');
-  const nameArg = args.findIndex((arg) => arg === '--name' || arg === '-n');
-  const seasonArg = args.findIndex((arg) => arg === '--season' || arg === '-s');
-  const offsetArg = args.findIndex((arg) => arg === '--offset' || arg === '-o');
-  dir = args[dirArg + 1];
-  name = args[nameArg + 1];
-  season = args[seasonArg + 1];
-  if (offsetArg > 0) {
-    epOffset = Number(args[offsetArg + 1]);
-  } else {
-    epOffset = 0;
-  }
-  if (!dirArg || !nameArg || !seasonArg || args.length < 6) {
-    exitInvalidInput();
+  runNormally();
+}
+
+function runNormally() {
+  const { dir, name, season, preview } = opts;
+  validateOptions(dir, name);
+  renameFilesInFolder(dir, name, season, preview);
+}
+
+async function runInteractively() {
+  const questions = [
+    {
+      message: 'Video folder path: ',
+      type: 'text',
+      name: 'dir',
+      initial: opts.folder,
+    },
+    {
+      message: 'Series name',
+      type: 'text',
+      name: 'name',
+      initial: opts.name,
+    },
+    {
+      message: 'Season',
+      type: 'number',
+      name: 'season',
+      initial: opts.season,
+    },
+  ];
+  const response = await prompts(questions);
+  const { dir, name, season } = response;
+  const { preview } = opts;
+  validateOptions(dir, name);
+  renameFilesInFolder(dir, name, season, preview);
+}
+
+function validateOptions(dir, name) {
+  if (!dir || !name) {
+    program.help();
   }
 }
 
-const isPreview = !!args.find((arg) => arg === '--preview' || arg === '-p');
+function renameFilesInFolder(dir, name, season, preview) {
+  console.log(`${preview ? 'PREVIEW ' : ''}Running renaming on path:
+    ${dir}
+    with name:
+    ${name}
+    `);
 
-if (!dir || !name || !season) {
-  exitInvalidInput();
+  const files = fs.readdirSync(dir);
+  files.forEach((fileName, index) => {
+    if (!isVideo(fileName)) {
+      return;
+    }
+    const episode = pad(getEpisodeNo(fileName) || index, 2);
+    renameFile(fileName, episode, dir, name, season, preview);
+  });
 }
 
-console.log(`${isPreview && 'PREVIEW'} Running renaming on path: 
-  ${dir}
-  with name:
-  ${name}
-  offset: ${epOffset}
-  `);
+function renameFile(currentFileName, episode, dir, name, season, preview) {
+  const fileEnding = getFileExtension(currentFileName);
 
-const files = fs.readdirSync(dir);
-files.forEach((fileName, index) => {
-  const fileEnding = fileEndings.filter((ending) =>
-    fileName.includes(ending)
-  )[0];
+  const filePath = dir + '/' + currentFileName;
+  const newFileName = buildFileName(name, season, episode, fileEnding);
+  const newFilePath = `${dir}/${newFileName}`;
 
-  const fileNo = pad(index + epOffset + 1, 2);
-  const filePath = dir + '/' + fileName;
-  const newFileName = `${name} S${season}E${fileNo}.${fileEnding}`;
-  const newFile = `${dir}/${newFileName}`;
-
-  console.info(`Renaming ${fileName} to ${newFileName}`);
-  if (isPreview) {
+  console.info(`Renaming ${currentFileName} to ${newFileName}`);
+  if (preview) {
     return;
   } else {
-    fs.renameSync(filePath, newFile);
+    fs.renameSync(filePath, newFilePath);
   }
-});
-
-function exitInvalidInput() {
-  console.error('Invalid inputs. arguments: --folder|-f --name|-n --season|-s');
-  exit();
 }
 
+function buildFileName(name, season, episode, fileEnding) {
+  let fileName = `${name} `;
+  if (season && season !== 0) {
+    fileName = `${fileName}S${season}`;
+  }
+  if (episode && episode !== 0) {
+    fileName = `${fileName}E${episode}`;
+  }
+  fileName = `${fileName}${fileEnding}`;
+  return fileName;
+}
 function pad(n, width, z) {
   z = z || '0';
   n = n + '';
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+function isVideo(fileName) {
+  for (const reg of EP_REGS) {
+    if (reg.test(fileName) && FILE_EXTENSIONS_REG.test(fileName)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getEpisodeNo(fileName) {
+  for (const reg of EP_REGS) {
+    let match = reg.exec(fileName);
+    if (match && match.length > 0 && match.groups) {
+      return match.groups.episode;
+    }
+  }
+}
+
+function getFileExtension(fileName) {
+  const match = FILE_EXTENSIONS_REG.exec(fileName);
+  if (match && match.length > 0) {
+    return match[0];
+  }
 }
