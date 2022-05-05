6fb53bce56da7ddfc87636091c8aaced920869ab
proohit <direnc99@gmail.com>
2022-05-05 12:57:53 +0200

    feat: waiting for finish before attempting to move

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
 #!/usr/bin/env node
 
 import { watch } from "chokidar";
+import _ from "lodash";
 import { checkAnidbDb, searchAnime } from "./anidb.js";
+import { loadConfig } from "./config.js";
 import {
   buildEpisodeFileName,
+  buildMovieFileName,
+  copyFile,
   createFolder,
   fileExists,
   getFileExtension,
-  copyFile,
-  removeFile,
-  getSubDirs,
+  getFileNameOfPath,
   getFilesOfDir,
-  buildMovieFileName,
+  getParentFolderName,
+  isFolderStillLoading,
+  removeFile,
 } from "./file-utils.js";
 import { logger } from "./logger.js";
-import { loadConfig } from "./config.js";
 import {
   createAnidbFile,
   getEpisodeNo,
-  getMovieOfFolder,
   getSeasonNo,
   isFolderAnimeMovie,
   isVideo,
 } from "./media-utils.js";
-
-import debounce from "lodash/debounce.js";
 
 const APP_CONFIG_FILE_PATH = process.argv
   .find((arg) => arg.includes("config-file"))
   ?.split("=")[1];
 
 let appConfig = undefined;
-run();
 const fileJobStatus = new Map();
+run();
+
+const getDebouncedByType = (func, wait, options) => {
+  const memory = {};
+
+  return (...args) => {
+    // use first argument as a key
+    // its possible to use all args as a key - e.g JSON.stringify(args) or hash(args)
+    const [searchType] = args;
+
+    if (typeof memory[searchType] === "function") {
+      return memory[searchType](...args);
+    }
+
+    memory[searchType] = _.debounce(func, wait, { ...options, leading: true }); // leading required for return promise
+    return memory[searchType](...args);
+  };
+};
+
+const debouncedHandleFileAdded = getDebouncedByType(handleFileAdded, 3000);
 
 async function run() {
   appConfig = await loadConfig(APP_CONFIG_FILE_PATH);
@@ -42,7 +61,45 @@ async function run() {
   watch(`${inputFolder}/*/**`, {
     ignoreInitial: false,
     awaitWriteFinish: true,
-  }).on("add", debounce(moveAllFiles, 3000));
+  }).on("all", async (eventName, path) => {
+    if (eventName === "add" || eventName === "change") {
+      const fileName = getFileNameOfPath(path);
+      const parentFolder = getParentFolderName(path);
+      await debouncedHandleFileAdded(parentFolder, fileName, path);
+    }
+  });
+}
+
+async function handleFileAdded(parentFolderName, fileName, fullSourcePath) {
+  logger.debug(`File added: ${fullSourcePath}`);
+  const pathOfFile = fullSourcePath.replace(`/${fileName}`, "").trim();
+  if (await isFolderStillLoading(pathOfFile)) {
+    logger.debug(`${pathOfFile} is still loading. Waiting for it to finish.`);
+    return;
+  }
+  if (isFolderAnimeMovie(parentFolderName)) {
+    if (fileName) {
+      logger.debug(
+        `Attempting to move movie file: ${fileName} from ${fullSourcePath}`
+      );
+      await moveMovieFile(parentFolderName, fileName, fullSourcePath);
+    }
+  } else {
+    if (fileJobStatus.has(pathOfFile)) {
+      logger.debug(`Series ${pathOfFile} is currently being processed`);
+      return;
+    } else {
+      fileJobStatus.set(pathOfFile, true);
+      const filesOfSeries = await getFilesOfDir(pathOfFile);
+      for (const file of filesOfSeries) {
+        logger.debug(
+          `Attempting to move episode file: ${fileName} from ${fullSourcePath}`
+        );
+        await moveEpisodeFile(parentFolderName, getFileNameOfPath(file), file);
+      }
+      fileJobStatus.delete(pathOfFile);
+    }
+  }
 }
 
 function getFileParts(fileName) {
@@ -55,34 +112,14 @@ function getFileParts(fileName) {
   return { episode, season, extension };
 }
 
-async function moveAllFiles() {
-  const { inputFolder } = appConfig;
-  const subDirs = await getSubDirs(inputFolder);
-  for (const subDir of subDirs) {
-    const subDirName = subDir.split("/")[subDir.split("/").length - 1];
-    if (isFolderAnimeMovie(subDirName)) {
-      const movieFile = await getMovieOfFolder(subDir);
-      if (movieFile) {
-        logger.info(
-          `Attempting to move movie file: ${movieFile} from ${subDir}`
-        );
-        await moveMovieFile(subDir, movieFile);
-      }
-    } else {
-      const files = await getFilesOfDir(subDir);
-      for (const file of files) {
-        logger.info(`Attempting to move ${file} from ${subDir}`);
-        await moveEpisodeFile(file);
-      }
-    }
-  }
-}
-
-async function moveMovieFile(subDir, movieFile) {
-  const splitPath = subDir.split("/");
-  const movieName = splitPath[splitPath.length - 1]
-    .replace("ANIME-MOVIE", "")
-    .trim();
+/**
+ *
+ * @param {string} subDirOfMovie name of the parent folder of the movie
+ * @param {string} movieFile name of the movie
+ * @param {string} fullSourcePath full source path of the file
+ */
+async function moveMovieFile(subDirOfMovie, movieFile, fullSourcePath) {
+  const movieName = subDirOfMovie.replace("ANIME-MOVIE", "").trim();
   if (isVideo(movieFile)) {
     const fileParts = getFileParts(movieFile);
     const newFileName = buildMovieFileName(movieName, fileParts.extension);
@@ -91,22 +128,21 @@ async function moveMovieFile(subDir, movieFile) {
       outputFolder,
       movieName,
       newFileName,
-      movieFile
+      fullSourcePath
     );
   } else {
     logger.debug(`Ignoring, not a video`);
   }
 }
 
-async function moveEpisodeFile(path) {
-  logger.debug(`File added: ${path}`);
-  const splitPath = path.split("/");
-  const fileName = splitPath[splitPath.length - 1];
-  let parentFolderName = "";
-  if (splitPath.length > 1) {
-    parentFolderName = splitPath[splitPath.length - 2];
-  }
-  if (isVideo(fileName) && !fileJobStatus.has(path)) {
+/**
+ *
+ * @param {string} parentFolderName name of the parent folder of the file
+ * @param {string} fileName name of the file
+ * @param {string} fullSourcePath full source path of the file
+ */
+async function moveEpisodeFile(parentFolderName, fileName, fullSourcePath) {
+  if (isVideo(fileName) && !fileJobStatus.has(fullSourcePath)) {
     try {
       const matchedAnimeName = parentFolderName;
       const { episode, season, extension } = getFileParts(fileName);
@@ -125,7 +161,7 @@ async function moveEpisodeFile(path) {
         outputFolder,
         matchedAnimeName,
         newFileName,
-        path
+        fullSourcePath
       );
     } catch (e) {
       logger.error(e.message);
@@ -147,8 +183,8 @@ async function moveFileWithPreparations(
   const newPath = `${newParent}/${newFileName}`;
   logger.info(`Moving ${sourcePath} to ${newPath}`);
   fileJobStatus.set(sourcePath, true);
-  await copyFile(sourcePath, newPath);
-  await removeFile(sourcePath);
+  copyFile(sourcePath, newPath);
+  removeFile(sourcePath);
   fileJobStatus.delete(sourcePath);
   const shouldReloadConfig = await checkAnidbDb(
     APP_CONFIG_FILE_PATH,
